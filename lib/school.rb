7c1fa64f84f733cb39b5a3406b0c043e320525e1
Tak H Kim <takhyun818@gmail.com>
2018-11-08 19:20:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialized(name)
+    @name = name
+    @roster = {}
+  end
+end
+
+def add_student(name, grade)
+  @roster[grade] ||= []
+  @roster[grade] << name
+  @roster
+end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
